318883d498a68ed0727cb2025fd77f118b00f81e
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-02-13 09:31:43 +0100

    fix(lualine): increase contrasts for 'c' sections

--- lua/lualine/themes/pastelnight.lua
+++ lua/lualine/themes/pastelnight.lua
@@ -6,43 +6,43 @@ local pastelnight = {}
 pastelnight.normal = {
   a = { bg = colors.purple, fg = colors.bg, gui = 'bold' },
   b = { bg = colors.bg_statusline, fg = colors.purple300 },
-  c = { bg = colors.bg_statusline, fg = colors.base400 },
+  c = { bg = colors.bg_statusline, fg = colors.base300 },
 }
 
 pastelnight.insert = {
   a = { bg = colors.blue, fg = colors.bg, gui = 'bold' },
   b = { bg = colors.bg_statusline, fg = colors.blue300 },
-  c = { bg = colors.bg_statusline, fg = colors.base400 },
+  c = { bg = colors.bg_statusline, fg = colors.base300 },
 }
 
 pastelnight.command = {
   a = { bg = colors.purple, fg = colors.bg, gui = 'bold' },
   b = { bg = colors.bg_statusline, fg = colors.purple300 },
-  c = { bg = colors.bg_statusline, fg = colors.base400 },
+  c = { bg = colors.bg_statusline, fg = colors.base300 },
 }
 
 pastelnight.visual = {
   a = { bg = colors.yellow, fg = colors.bg, gui = 'bold' },
   b = { bg = colors.bg_statusline, fg = colors.yellow300 },
-  c = { bg = colors.bg_statusline, fg = colors.base400 },
+  c = { bg = colors.bg_statusline, fg = colors.base300 },
 }
 
 pastelnight.replace = {
   a = { bg = colors.green, fg = colors.bg, gui = 'bold' },
   b = { bg = colors.bg_statusline, fg = colors.green300 },
-  c = { bg = colors.bg_statusline, fg = colors.base400 },
+  c = { bg = colors.bg_statusline, fg = colors.base300 },
 }
 
 pastelnight.terminal = {
   a = { bg = colors.pink, fg = colors.bg, gui = 'bold' },
   b = { bg = colors.bg_statusline, fg = colors.pink300 },
-  c = { bg = colors.bg_statusline, fg = colors.base400 },
+  c = { bg = colors.bg_statusline, fg = colors.base300 },
 }
 
 pastelnight.inactive = {
   a = { bg = colors.bg_statusline, fg = colors.purple, gui = 'bold' },
-  b = { bg = colors.bg_statusline, fg = colors.base400 },
-  c = { bg = colors.bg_statusline, fg = colors.base400 },
+  b = { bg = colors.bg_statusline, fg = colors.base300 },
+  c = { bg = colors.bg_statusline, fg = colors.base300 },
 }
 
 if config.lualine_bold then
