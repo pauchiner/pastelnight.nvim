aebafc02c3f19445d9e23d59d1226e6895b2c16c
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-02-14 09:25:32 +0100

    style(theme): update config comments to be more legible

--- lua/pastelnight/config.lua
+++ lua/pastelnight/config.lua
@@ -4,26 +4,45 @@ local M = {}
 ---@field on_colors fun(colors: ColorScheme)
 ---@field on_highlights fun(highlights: Highlights, colors: ColorScheme)
 local defaults = {
-  style = 'storm', -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
-  light_style = 'day', -- The theme is used when the background is set to light
-  transparent = false, -- Enable this to disable setting the background color
-  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+  --- The theme comes in three styles, `storm`, a darker variant `night` and `day`.
+  style = 'storm',
+
+  --- The theme is used when the background is set to light.
+  light_style = 'day',
+
+  --- Enable this to disable setting the background color
+  transparent = false,
+
+  --- Configure the colors used when opening a `:terminal` in Neovim
+  terminal_colors = true,
+
+  --- Style to be applied to different syntax groups
+  --- Value is any valid attr-list value for `:help nvim_set_hl`
   styles = {
-    -- Style to be applied to different syntax groups
-    -- Value is any valid attr-list value for `:help nvim_set_hl`
     comments = { italic = true },
     keywords = { italic = true },
     functions = {},
     variables = {},
+
     -- Background styles. Can be "dark", "transparent" or "normal"
-    sidebars = 'normal', -- style for sidebars, see below
-    floats = 'dark', -- style for floating windows
+    sidebars = 'normal',
+    floats = 'dark',
   },
-  sidebars = { 'qf', 'help' }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-  day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-  hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-  dim_inactive = false, -- dims inactive windows
-  lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+
+  --- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+  sidebars = { 'qf', 'help' },
+
+  --- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors.
+  day_brightness = 0.3,
+
+  --- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+  hide_inactive_statusline = false,
+
+  --- Dims inactive windows.
+  dim_inactive = false,
+
+  --- When `true`, section headers in the lualine theme will be bold.
+  lualine_bold = false,
 
   --- You can override specific color groups to use other groups or a hex color
   --- function will be called with a ColorScheme table
